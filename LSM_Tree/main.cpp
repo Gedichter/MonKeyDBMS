423c27a6a42bdea143684f3cc22987f74654c971
Gedichter <shiyuhuang@g.harvard.edu>
2018-02-21 15:29:59 -0500

    Initial Commit

---
+++ LSM_Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LSM_Tree
+//
+//  Created by Shiyu Huang on 2/21/18.
+//  Copyright © 2018 Shiyu Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
